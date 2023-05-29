c971ffbc7ee9ed9e5de08f18b6bc17a43330e671
Vladimir <gcvqa@mail.ru>
2023-05-30 00:48:26 +1000

    likes 2

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
-class Posts::LikesController < Posts::ApplicationController
-  before_action :authenticate_user!
+module Posts
+  class LikesController < ApplicationController
+    before_action :authenticate_user!
 
-  def create
-    @post_like = resource_post.likes.create(user_id: current_user.id)
+    def create
+      @post_like = resource_post.likes.create(user_id: current_user.id)
 
-    # Just refresh current page or redirect to to root if refferer is unknown
-    redirect_back(fallback_location: root_path)
-  end
+      # Just refresh current page or redirect to to root if refferer is unknown
+      redirect_back(fallback_location: root_path)
+    end
 
-  def destroy
-    @post_like = resource_post.likes.find_by(user: current_user)&.destroy
+    def destroy
+      @post_like = resource_post.likes.find_by(user: current_user)&.destroy
 
-    redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path)
+    end
+
+
   end
 end
+
+
