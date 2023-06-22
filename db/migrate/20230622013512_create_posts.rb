1c1d119ea54b0f3b627e070dae6e6f85d0aabe5a
Oleg Bavaev <csolg7@gmail.com>
2023-06-22 17:44:08 +1000

    Fix migration

--- db/migrate/20230622013512_create_posts.rb
+++ db/migrate/20230622013512_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.belongs_to :category, null: false, foreign_key: true
-      t.belongs_to :creator, null: false, foreign_key: true
+      t.belongs_to :creator, null: false, foreign_key: { to_table: :users }
       t.string :title
       t.text :body
 
