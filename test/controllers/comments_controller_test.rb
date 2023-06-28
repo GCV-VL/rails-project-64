0f60ceb1a4c5fdb0b2ef3accdb6fad79e617aca5
Oleg Bavaev <csolg7@gmail.com>
2023-06-28 14:28:19 +1000

    Add test for CommentsController

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    sign_in(users(:one))
+  end
+
+  test 'should create comment' do
+    post = posts(:one)
+    comment_params = { post: post, content: FFaker::Lorem.paragraph(10) }
+    post post_post_comments_url(post), params: { post_comment: comment_params }
+    assert_redirected_to post_url(post)
+  end
 end
