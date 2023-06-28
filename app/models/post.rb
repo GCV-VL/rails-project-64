7d691c14e23c826cd5ae22274fec3558621cb50d
Kim Vladimir <kimvova85@mail.ru>
2023-06-28 13:39:26 +1000

    Add comments_controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :creator, class_name: 'User'
 
+  has_many :comments, -> { order(updated_at: :desc) }, class_name: 'PostComment'
+
   validates :title, presence: true, length: { in: 5..255 }
   validates :category, presence: true
   validates :body, presence: true, length: { in: 200..4000 }
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resources :posts
+  resources :posts do
+    resources :post_comments, controller: 'comments'
+  end
 end
