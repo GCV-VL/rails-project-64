7ed818ae36cc264a482e434de7efafc528ee3eb1
Kim Vladimir <kimvova85@mail.ru>
2023-07-19 18:56:48 +1000

    Fix PostsController#show as guest

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,44 +2,53 @@
 
 require 'test_helper'
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-
-  setup do
-    sign_in(users(:one))
+module PostsControllerTest
+  class AsGuest < ActionDispatch::IntegrationTest
+    test 'should get show' do
+      get post_url(posts(:one))
+      assert_response :success
+    end
   end
 
-  test 'should get new' do
-    get new_post_url
-    assert_response :success
-  end
+  class AsSignedUser < ActionDispatch::IntegrationTest
+    include Devise::Test::IntegrationHelpers
 
-  test 'should get edit' do
-    get edit_post_url(posts(:one))
-    assert_response :success
-  end
+    setup do
+      sign_in(users(:one))
+    end
 
-  test 'should get show' do
-    get post_url(posts(:one))
-    assert_response :success
-  end
+    test 'should get new' do
+      get new_post_url
+      assert_response :success
+    end
 
-  test 'should create post' do
-    post_params = { creator: users(:one), title: Faker::Lorem.sentence, category_id: categories(:one).id, body: Faker::Lorem.paragraphs(number: 10).join("\n") }
-    post posts_url, params: { post: post_params }
-    assert_redirected_to post_url(Post.last)
-  end
+    test 'should get edit' do
+      get edit_post_url(posts(:one))
+      assert_response :success
+    end
 
-  test 'should update post' do
-    title = Faker::Lorem.sentence
-    body = Faker::Lorem.paragraphs(number: 10).join("\n")
-    post = posts(:one)
-    post_params = { creator: users(:one), title:, category_id: categories(:one).id, body: }
+    test 'should get show' do
+      get post_url(posts(:one))
+      assert_response :success
+    end
 
-    put post_url(post), params: { post: post_params }
+    test 'should create post' do
+      post_params = { creator: users(:one), title: Faker::Lorem.sentence, category_id: categories(:one).id, body: Faker::Lorem.paragraphs(number: 10).join("\n") }
+      post posts_url, params: { post: post_params }
+      assert_redirected_to post_url(Post.last)
+    end
 
-    assert_redirected_to post_url(post)
-    assert_equal(title, post.reload.title)
-    assert_equal(body, post.reload.body)
+    test 'should update post' do
+      title = Faker::Lorem.sentence
+      body = Faker::Lorem.paragraphs(number: 10).join("\n")
+      post = posts(:one)
+      post_params = { creator: users(:one), title:, category_id: categories(:one).id, body: }
+
+      put post_url(post), params: { post: post_params }
+
+      assert_redirected_to post_url(post)
+      assert_equal(title, post.reload.title)
+      assert_equal(body, post.reload.body)
+    end
   end
 end
